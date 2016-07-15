53bb93d1083d097fd937e8ed2a75ef34a23fa7f0
“Jailman <sololobo@foxmail.com>
2016-07-15 13:29:28 +0800

    添加网络iosql和agent函数

--- web/agent.py
+++ web/agent.py
@@ -48,6 +48,16 @@ class dioThread (threading.Thread):
         diskio_mon()
 
 
+class nioThread (threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        nicio_mon()
+
+
+
 def cpu_mon():
     cx = sqlite3.connect("falcon.db")
     cur = cx.cursor()
@@ -150,12 +160,46 @@ def diskio_mon():
         time.sleep(5)
 
 
+def nicio_mon():
+    cx = sqlite3.connect("falcon.db")
+    cur = cx.cursor()
+    while True:
+        nio_data = {}
+        nio_data['Host'] = host
+        nio_data['Time'] = int(int(time.time()) + 8 * 3600)
+        sent_start_byte = psutil.net_io_counters()[0]
+        recv_start_byte = psutil.net_io_counters()[1]
+        sent_start_pkt = psutil.net_io_counters()[2]
+        recv_start_pkt = psutil.net_io_counters()[3]
+        time.sleep(1)
+        sent_end_byte = psutil.net_io_counters()[0]
+        recv_end_byte = psutil.net_io_counters()[1]
+        sent_end_pkt = psutil.net_io_counters()[2]
+        recv_end_pkt = psutil.net_io_counters()[3]
+        nio_data["Sent_speed_kb"] = int(sent_end_byte - sent_start_byte)/1024
+        nio_data["Recv_speed_kb"] = int(recv_end_byte - recv_start_byte)/1024
+        nio_data["Sent_speed_pkt"] = int(sent_end_pkt - sent_start_pkt)
+        nio_data["Recv_speed_pkt"] = int(recv_end_pkt - recv_start_pkt)
+        print nio_data
+        try:
+            sql = "INSERT INTO `nicio_stat` (`host`, `sent_speed_kb`, `recv_speed_kb`, `sent_speed_pkt`, `recv_speed_pkt`, `time`) VALUES('%s', '%d', '%d', '%d', '%d', '%d')" % (
+                nio_data['Host'], nio_data["Sent_speed_kb"], nio_data["Recv_speed_kb"], nio_data["Sent_speed_pkt"], nio_data["Recv_speed_pkt"], int(nio_data['Time']))
+            cur.execute(sql)
+            cx.commit()
+        except:
+            pass
+        print "Nicio DB insert OK"
+        time.sleep(5)
+
+
 if __name__ == "__main__":
     cputhread = cpuThread()
     memthread = memThread()
     dcThread = dcThread()
     dioThread = dioThread()
+    nioThread = nioThread()
     cputhread.start()
     memthread.start()
     dcThread.start()
     dioThread.start()
+    nioThread.start()
--- web/io.py
+++ web/io.py
@@ -9,48 +9,47 @@ import threading
 
 host = socket.gethostname()
 
-class dioThread (threading.Thread):
+class nioThread (threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
-        diskio_mon()
+        nicio_mon()
 
 
-def diskio_mon():
+def nicio_mon():
     cx = sqlite3.connect("falcon.db")
     cur = cx.cursor()
     while True:
-        dio_data = {}
-        dio_data['Host'] = host
-        dio_data['Time'] = int(int(time.time()) + 8 * 3600)
-        read_start_count = psutil.disk_io_counters()[0]
-        write_start_count = psutil.disk_io_counters()[1]
-        read_start_byte = psutil.disk_io_counters()[2]
-        write_start_byte = psutil.disk_io_counters()[3]
+        nio_data = {}
+        nio_data['Host'] = host
+        nio_data['Time'] = int(int(time.time()) + 8 * 3600)
+        sent_start_byte = psutil.net_io_counters()[0]
+        recv_start_byte = psutil.net_io_counters()[1]
+        sent_start_pkt = psutil.net_io_counters()[2]
+        recv_start_pkt = psutil.net_io_counters()[3]
         time.sleep(1)
-        read_end_count = psutil.disk_io_counters()[0]
-        write_end_count = psutil.disk_io_counters()[1]
-        read_end_byte = psutil.disk_io_counters()[2]
-        write_end_byte = psutil.disk_io_counters()[3]
-        dio_data["Read_speed_count"] = int(read_end_count - read_start_count)
-        dio_data["Write_speed_count"] = int(write_end_count - write_start_count)
-        dio_data["Read_speed_kb"] = int(read_end_byte - read_start_byte)
-        dio_data["Write_speed_kb"] = int(write_end_byte - write_start_byte)
-        dio_data["Read_time"] = int(psutil.disk_io_counters()[4])
-        dio_data["Write_time"] = int(psutil.disk_io_counters()[5])
-        print dio_data
+        sent_end_byte = psutil.net_io_counters()[0]
+        recv_end_byte = psutil.net_io_counters()[1]
+        sent_end_pkt = psutil.net_io_counters()[2]
+        recv_end_pkt = psutil.net_io_counters()[3]
+        nio_data["Sent_speed_kb"] = int(sent_end_byte - sent_start_byte)/1024
+        nio_data["Recv_speed_kb"] = int(recv_end_byte - recv_start_byte)/1024
+        nio_data["Sent_speed_pkt"] = int(sent_end_pkt - sent_start_pkt)
+        nio_data["Recv_speed_pkt"] = int(recv_end_pkt - recv_start_pkt)
+        print nio_data
         try:
-            sql = "INSERT INTO `diskio_stat` (`host`, `read_speed_count`, `write_speed_count`, `read_speed_kb`, `write_speed_kb`, `read_time`, `write_time`, `time`) VALUES('%s', '%d', '%d', '%d', '%d', '%d', '%d', '%d')" % (
-                dio_data['Host'], dio_data["Read_speed_count"], dio_data["Write_speed_count"], dio_data["Read_speed_kb"], dio_data["Write_speed_kb"], dio_data["Read_time"], dio_data["Write_time"], int(dio_data['Time']))
+            sql = "INSERT INTO `nicio_stat` (`host`, `sent_speed_kb`, `recv_speed_kb`, `sent_speed_pkt`, `recv_speed_pkt`, `time`) VALUES('%s', '%d', '%d', '%d', '%d', '%d')" % (
+                nio_data['Host'], nio_data["Sent_speed_kb"], nio_data["Recv_speed_kb"], nio_data["Sent_speed_pkt"], nio_data["Recv_speed_pkt"], int(nio_data['Time']))
             cur.execute(sql)
             cx.commit()
         except:
             pass
-        print "Diskio DB insert OK"
+        print "Nicio DB insert OK"
         time.sleep(5)
 
+
 if __name__ == "__main__":
-    dioThread = dioThread()
-    dioThread.start()
+    nioThread = nioThread()
+    nioThread.start()
